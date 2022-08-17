6057890a86bbd024b6eff355a9903f66d192ec3b
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-08-17 18:14:49 +0300

    commit tree 973e19e19d6e69c39a87b6e5735c1134d3152ddc

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(number)
+   while number >=1 
+    puts "#{number} SECOND(S)!"
+      sleep 1
+      number -=1
+   end
+   return   'HAPPY NEW YEAR!'
 end
+# countdown(10)
